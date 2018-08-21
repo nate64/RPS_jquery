9002e8412f8b23b82cee7ec05ca051e3007342e8
Nate <nathanestes@gmail.com>
2018-08-21 17:09:04 -0600

    first and last commit

---
+++ main.js
@@ -0,0 +1,64 @@
+$(document).ready( function() {
+
+  //variables
+
+  var rock = $('#Rock')
+  var paper = $('#Paper')
+  var scissors = $('#Scissors')
+  var userChoice
+  var userScore
+  var compScore
+  var compChoice
+  var draw
+  var compare
+  var rPs = ['Rock', 'Paper', 'Scissors']
+
+
+  //Functions
+    
+  rock.on('click', function() {
+    userChoice = 'Rock'
+    compChoice = rPs[Math.floor(Math.random() * rPs.length)];
+    console.log (userChoice)
+    console.log (compChoice)
+    var compare = evaluate()
+    console.log(compare)
+    //increment score??
+    })
+
+  paper.on('click', function(){
+    userChoice = 'Paper'
+    compChoice = rPs[Math.floor(Math.random() * rPs.length)];
+    console.log (userChoice)
+    console.log (compChoice)
+    var compare = evaluate()
+    console.log(compare)
+  })
+
+  scissors.on('click', function(){
+    userChoice = 'Scissors'
+    compChoice = rPs[Math.floor(Math.random() * rPs.length)];
+    console.log (userChoice)
+    console.log (compChoice)
+    var compare = evaluate()
+    console.log(compare)
+  })
+
+  function evaluate() {
+    if (userChoice === compChoice)
+      return 'tie'
+
+    switch(userChoice) {
+      case 'Rock':
+        return compChoice === 'paper' ? 'lose' : 'win'
+      case 'Paper':
+        return compChoice === 'scissors' ? 'lose' : 'win'
+      case 'Scissors':
+        return compChoice === 'rock' ? 'lose' : 'win'
+      default:
+        return
+    }
+  }
+
+  
+});

5a1a61d7f310fb1237cad2761d87483fb7586caf
Nate <nathanestes@gmail.com>
2018-08-21 22:04:41 -0600

    last one, I promise

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 $(document).ready( function() {
 
   //variables
-
   var rock = $('#Rock')
   var paper = $('#Paper')
   var scissors = $('#Scissors')
   var userChoice
-  var userScore
-  var compScore
+  var userScore = 0
+  var compScore = 0
   var compChoice
-  var draw
+  var draw = 0
   var compare
   var rPs = ['Rock', 'Paper', 'Scissors']
 
-
   //Functions
-    
   rock.on('click', function() {
     userChoice = 'Rock'
     compChoice = rPs[Math.floor(Math.random() * rPs.length)];
@@ -23,9 +20,13 @@ $(document).ready( function() {
     console.log (compChoice)
     var compare = evaluate()
     console.log(compare)
-    //increment score??
-    if compare = win, increment userScore ++1
-    else increment compScore ++ 1
+    alert('You chose ' + userChoice + ' And The Computer Chose: ' + compChoice + '.')
+    //debugger
+    if (compare = 'Win') {
+      userScore += 1
+    } else {
+        compScore += 1
+    }
     })
 
   paper.on('click', function(){
@@ -35,6 +36,12 @@ $(document).ready( function() {
     console.log (compChoice)
     var compare = evaluate()
     console.log(compare)
+    alert('You chose ' + userChoice + ' And The Computer Chose: ' + compChoice + '.')
+    if (compare = 'Win') {
+      userScore += 1
+    } else {
+        compScore += 1
+    }
   })
 
   scissors.on('click', function(){
@@ -44,23 +51,33 @@ $(document).ready( function() {
     console.log (compChoice)
     var compare = evaluate()
     console.log(compare)
+    alert('You chose ' + userChoice + ' And The Computer Chose: ' + compChoice + '.')
+    if (compare = 'Win') {
+      userScore += 1
+    } else {
+        compScore += 1
+    }
   })
 
   function evaluate() {
     if (userChoice === compChoice)
       return 'tie'
+      draw += 1 
 
     switch(userChoice) {
       case 'Rock':
-        return compChoice === 'paper' ? 'lose' : 'win'
+        return compChoice === 'paper' ? 'Lose' : 'Win'
       case 'Paper':
-        return compChoice === 'scissors' ? 'lose' : 'win'
+        return compChoice === 'scissors' ? 'Lose' : 'Win'
       case 'Scissors':
-        return compChoice === 'rock' ? 'lose' : 'win'
+        return compChoice === 'rock' ? 'Lose' : 'Win'
       default:
         return
     }
   }
 
+  document.getElementById('userScore').innerHTML = "Player 1: " + userScore
+  document.getElementById('compScore').innerHTML = "Computer: " + compScore
+  document.getElementById('draw').innerHTML = "Draws: " + draw
   
 });
